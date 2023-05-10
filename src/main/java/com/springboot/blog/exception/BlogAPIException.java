e6fad13e43c3e3bf9064dd16d9ca01a084d90448
DidierSML <didiersmaldonad@gmail.com>
2023-05-09 19:23:20 -0500

    Working on Comment layers

---
+++ src/main/java/com/springboot/blog/exception/BlogAPIException.java
@@ -0,0 +1,21 @@
+package com.springboot.blog.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+//We throw this exception whenever we write some business logic or validate request parameters
+@Getter
+@AllArgsConstructor
+public class BlogAPIException extends RuntimeException {
+
+    private HttpStatus status;
+    private String message;
+}
+
+/*We used @AllArgumentsConstructor, the same form is possible generate
+ Constructor just for -RunTimeException() y RunTimeException(message:String)
+
+ Also, methods @Getter have been created using lombok establishing access to them.
+
+ */
--- src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/CommentServiceImpl.java
@@ -4,12 +4,17 @@ import com.springboot.blog.dto.CommentDto;
 import com.springboot.blog.dto.PostDto;
 import com.springboot.blog.entity.Comment;
 import com.springboot.blog.entity.Post;
+import com.springboot.blog.exception.BlogAPIException;
 import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.repository.CommentRepository;
 import com.springboot.blog.repository.PostRepository;
 import com.springboot.blog.service.CommentService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -20,7 +25,7 @@ public class CommentServiceImpl implements CommentService {
 
     private final PostRepository postRepository;
 
-    @Override
+    @Override //Logic for (create/save) Comment
     public CommentDto createComment(long postId, CommentDto commentDto) {
 
         Comment comment = mapToEntity(commentDto);
@@ -31,11 +36,87 @@ public class CommentServiceImpl implements CommentService {
         //set post to comment entity
         comment.setPost(post);
 
-        //comment Entity to DB
+        //comment Entity to DB -invoking save method provided by JPA
         Comment newComment = commentRepository.save(comment);
 
         return mapToDto(newComment);
     }
+
+    @Override //Logic for (getCommentsByPostId) All
+    public List<CommentDto> getCommentsByPostId(long postId) {
+
+        //retrieve comments by postId -invoking find method provided by JPA
+        List<Comment> comments = commentRepository.findByPostId(postId);
+
+        //convert list of comment Entities to List of comments Dto
+        return comments.stream().map(comment -> mapToDto(comment)).collect(Collectors.toList());
+    }
+
+    //Logic to get a Comment By Id
+    @Override
+    public CommentDto getCommentById(Long postId, Long commentId) {
+
+        //retrieve (post) Entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post","id", postId));
+
+        //retrieve (comment) Entity by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("Comment","id",commentId));
+
+        //Condition of searching -if comment(id) does not belong to post(id) throw ...
+        if(!comment.getPost().getId().equals(post.getId())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belong to post");
+        }
+
+        return mapToDto(comment);
+    }
+
+    //Logic to update a Comment
+    @Override
+    public CommentDto updateComment(long postId, long commentId, CommentDto commentRequest) {
+
+        //retrieve (post) Entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post","id", postId));
+
+        //retrieve (comment) Entity by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("Comment","id",commentId));
+
+        //Condition of searching -if comment(id) does not belong to post(id) throw ...
+        if(!comment.getPost().getId().equals(post.getId())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belong to post");
+        }
+
+        //Setting the new values for object Comment
+        comment.setName(commentRequest.getName());
+        comment.setEmail(commentRequest.getEmail());
+        comment.setBody(commentRequest.getBody());
+
+        //Saving new updated Comment Entity into DB -invoking save method provided by JPA
+        Comment updatedComment = commentRepository.save(comment);
+
+        //returning the Dto Comment Updated
+        return mapToDto(updatedComment);
+
+    }
+
+    //Logic to delete a Comment By Id
+    @Override
+    public void deleteCommentById(Long postId, Long commentId) {
+
+        //retrieve (post) Entity by id
+        Post post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post","id", postId));
+
+        //retrieve (comment) Entity by id
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new ResourceNotFoundException("Comment","id",commentId));
+
+        //Condition of searching -if comment(id) does not belong to post(id) throw ...
+        if(!comment.getPost().getId().equals(post.getId())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST, "Comment does not belong to post");
+        }
+        //Invoking the delete method provided by JPA
+        commentRepository.delete(comment);
+    }
+
+
 
     //private method to Convert (Entity To Dto)
     private CommentDto mapToDto(Comment comment){
