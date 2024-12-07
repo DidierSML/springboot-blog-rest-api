6fe4fb7a667e181584b684e17b9197778a7922f8
DidierSML <didiersmaldonad@gmail.com>
2024-12-07 16:46:46 -0500

    SecurityConfiguration

--- src/main/java/com/springboot/blog/config/SecurityConfiguration.java
+++ src/main/java/com/springboot/blog/config/SecurityConfiguration.java
@@ -4,12 +4,12 @@ package com.springboot.blog.config;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.web.SecurityFilterChain;
-//
+
 //import static org.springframework.security.config.Customizer.withDefaults;
-//
+
 //@Configuration
 //public class SecurityConfig {
-//
+
 //    @Bean
 //    SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
 //
